23ef25929638394adfffeb0bba90db7774f7223b
Username <user@example.com>
2022-02-27 00:55:21 +0100

    No addr vars

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-target=sum01 sum02
+target=sum01 sum02 cmp01 cmp02 hello01
 
 all:$(target)
 
--- sum02.s
+++ sum02.s
@@ -4,15 +4,12 @@
 .global main
 .balign 4
 main:
-    ldr r1, addr_of_myvar1 /* r1 ← &myvar1 */
-    ldr r1, [r1]           /* r1 ← *r1 */
-    ldr r2, addr_of_myvar2 /* r2 ← &myvar2 */
-    ldr r2, [r2]           /* r2 ← *r2 */
-    add r0, r1, r2         /* r0 ← r1 + r2 */
+    ldr r1,=myvar1      /* r1 ← &myvar1 */
+    ldr r1,[r1]         /* r1 ← *r1 */
+    ldr r2,=myvar2      /* r2 ← &myvar2 */
+    ldr r2,[r2]         /* r2 ← *r2 */
+    add r0,r1,r2        /* r0 ← r1 + r2 */
     bx lr
-
-addr_of_myvar1 : .word myvar1
-addr_of_myvar2 : .word myvar2
 
 .data
 .balign 4
