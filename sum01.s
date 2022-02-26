83a0f1981503383a4aa37e08f58a97242529a750
Username <user@example.com>
2022-02-27 00:03:20 +0100

    Use r0,r1

--- sum01.s
+++ sum01.s
@@ -2,7 +2,7 @@
 .global main
 
 main:
-    mov r1,#3      /* r1 ← 3 */
-    mov r2,#4      /* r2 ← 4 */
-    add r0,r1,r2   /* r0 ← r1 + r2 */
+    mov r0,#3      /* r1 ← 3 */
+    mov r1,#4      /* r2 ← 4 */
+    add r0,r0,r1   /* r0 ← r1 + r2 */
     bx  lr
