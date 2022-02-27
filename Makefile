613bff2515badfb9ba06cdd82df7f487295e3d10
Username <user@example.com>
2022-02-27 11:24:21 +0100

    AJoute cmp

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-target=sum01 sum02 cmp01 cmp02 hello01
+# target=sum01 sum02 cmp01 cmp02 hello01
+target = $(patsubst %.s,%,$(wildcard *.s))
 
 all:$(target)
 
--- sum02.s
+++ sum02.s
@@ -3,12 +3,13 @@
 /* Ensure code is 4 byte aligned */
 .global main
 .balign 4
+
 main:
-    ldr r1,=myvar1      /* r1 ← &myvar1 */
-    ldr r1,[r1]         /* r1 ← *r1 */
-    ldr r2,=myvar2      /* r2 ← &myvar2 */
-    ldr r2,[r2]         /* r2 ← *r2 */
-    add r0,r1,r2        /* r0 ← r1 + r2 */
+    ldr r0,=myvar1      /* r1 ← &myvar1 */
+    ldr r0,[r0]         /* r1 ← *r1 */
+    ldr r1,=myvar2      /* r2 ← &myvar2 */
+    ldr r1,[r1]         /* r2 ← *r2 */
+    add r0,r0,r1        /* r0 ← r1 + r2 */
     bx lr
 
 .data
