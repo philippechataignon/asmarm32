c04789ffda26d50301797302c2143cd71bdb23a3
Username <user@example.com>
2022-02-27 20:24:09 +0100

    Use push/pop

--- hello01.s
+++ hello01.s
@@ -1,21 +1,19 @@
 /* -- hello01.s */
+.syntax unified
+.code 32
 .global puts
 .global main
 
 .text
 main:
-            ldr r1,=return          /*   r1 ← &address_of_return */
-            str lr,[r1]             /*   *r1 ← lr */
-
+            push {lr}
             ldr r0,=greeting        /* r0 ← &address_of_greeting */
                                     /* First parameter of puts */
             bl puts                 /* Call to puts */
                                     /* lr ← address of next instruction */
-            ldr r1,=return          /* r1 ← &address_of_return */
-            ldr lr,[r1]             /* lr ← *r1 */
+            pop {lr}                /* lr ← *r1 */
             bx  lr                  /* return from main */
 
 .data
 .balign 4
-greeting:   .asciz "Hello world"
-return:     .word 0
+greeting:   .asciz "Hello world!"
